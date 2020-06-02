e30cbbdb2352c426360849dca3a736798ea6f548
DongHyun Kim <knight2995@users.noreply.github.com>
2020-06-02 12:20:31 +0900

    test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_echo_test
+
+go 1.13
+
+require github.com/labstack/echo/v4 v4.1.16 // indirect
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+
+	e := echo.New()
+	// 첫 화면
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World!")
+	})
+
+	e.GET("/text/:text", getText)
+	e.POST(("/post"), postHandler)
+
+	e.Logger.Fatal(e.Start(":80")) // localhost:1323
+}
+
+func getText(c echo.Context) error {
+
+	text := c.Param("text")
+	return c.String(http.StatusOK, text)
+}
+
+func postHandler(c echo.Context) error {
+
+	id := c.FormValue("id")
+	return c.String(http.StatusOK, id)
+}
